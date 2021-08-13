baf0597b4a003babb2225a7c1eb221f5d816e9be
MVelasquez98 <43156596+MVelasquez98@users.noreply.github.com>
2021-08-13 15:27:48 -0300

    cambios

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-var btnno = document.getElementById('btnno');
-var btnyes = document.getElementById('btnyes');
+var btnno = document.getElementById("btnno");
+var btnyes = document.getElementById("btnyes");
 
-btnno.addEventListener('mouseenter', () => {
-    console.log("mouse entrando");
-    var pos = coordenadas();
-    btnno.style.top = `${pos.posy}px`;
-    btnno.style.left = `${pos.posx}px`;
-})
+btnno.addEventListener("mouseenter", () => {
+  console.log("mouse entrando");
+  var pos = coordenadas();
+  btnno.style.top = `${pos.posy}px`;
+  btnno.style.left = `${pos.posx}px`;
+});
 
 var coordenadas = () => {
-    var ancho = screen.width - 100;
-    var alto = screen.height - 200;
-    var posx = Math.random() * (ancho - 0) + 0;
-    var posy = Math.random() * (alto - 0) + 0;
-    return { posx, posy }
-}
+  var ancho = screen.width - 100;
+  var alto = screen.height - 200;
+  var posx = Math.random() * (ancho - 0) + 0;
+  var posy = Math.random() * (alto - 0) + 0;
+  return { posx, posy };
+};
 
-btnyes.addEventListener('click', () => {
-    alert('Ya sabia que querias ser mi novia');
+btnyes.addEventListener("click", () => {
+  if (confirm("Ya sabia que querias ser mi novia jeje :) Te amo ")) {
+    var message = "Acepto Bebito Peshosho.";
+    window.open("https://wa.me/5491158559679/?text=" + encodeURI(message), "_blank");
+  }
 });
